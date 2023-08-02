ba1f079f412efc43e201dd3e5a1e7f94078fddd6
nooreekra <rakhymzhan.ff@gmail.com>
2023-08-02 16:53:09 +0600

    added react library

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -7,5 +7,17 @@ export function buildLoaders(): RuleSetRule[] {
     exclude: /node_modules/,
   };
 
-  return [typescriptLoader];
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from JS strings
+      "style-loader",
+      // Translates CSS into CommonJS
+      "css-loader",
+      // Compiles Sass to CSS
+      "sass-loader",
+    ],
+  };
+
+  return [typescriptLoader, cssLoader];
 }
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import { render } from "react-dom";
+import Counter from "./components/Counter";
+
+render (
+    <div><Counter /></div>, 
+    document.getElementById('root') 
+)
