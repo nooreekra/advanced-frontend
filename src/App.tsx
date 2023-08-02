73d698321b0c5a50ba76cc281010fa3438cfdece
nooreekra <rakhymzhan.ff@gmail.com>
2023-08-02 22:13:55 +0600

    added react router dom and with lazy suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import Counter from "./components/Counter";
-import './index.scss'
+import { Route, Routes } from "react-router-dom";
+import "./index.scss";
+import { Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Suspense } from "react";
 const App = () => {
   return (
     <div className="app">
-        Hello world!
-      <Counter />
+      <Link to={"/"}>Главная</Link>
+      <Link to={"/about"}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import { render } from "react-dom";
 import Counter from "./components/Counter";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-render(<App />, document.getElementById("root"));
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
