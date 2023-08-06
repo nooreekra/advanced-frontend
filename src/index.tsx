606fa227b3900eb9f39ab9aa19c209aa39ffefcd
nooreekra <rakhymzhan.ff@gmail.com>
2023-08-06 20:31:16 +0600

    added RTL for Sidebar and Button components

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,8 @@ import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'app/providers/ThemeProvider'
 import App from 'app/App'
 
-import 'shared/i18n/i18n'
+import 'shared/config/i18n/i18n'
 import { ErrorBoundary } from 'app/providers/ErrorBoundary'
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 
 render(
     <BrowserRouter>
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -20,11 +20,12 @@ export const Sidebar = ({ className }: SidebarProps) => {
 
   return (
       <div
+          data-testid={'sidebar'}
           className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [
             className
           ])}
-        >
-          <Button onClick={onToggle}>{t('toggle')}</Button>
+    >
+          <Button data-testid={'sidebar-toggle'} onClick={onToggle}>{t('toggle')}</Button>
           <div className={cls.switchers}>
               <ThemeSwitcher />
               <LangSwitcher className={cls.lang} />
